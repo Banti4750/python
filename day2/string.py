7374e589d6929b7b06d3f6907799e0ef05bf4d93
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-08 19:40:56 +0530

    string in python

---
+++ day2/string.py
@@ -0,0 +1,33 @@
+# course = "math jhasv asuihd auisuhdui"
+# print(course)
+
+# course = "math's jhasv asuihd auisuhdui"
+# print(course)
+
+# course = '''
+# math's jhasv asuihd auisuhdui
+# jhjbcj
+# kjnjkd
+# djdjh
+# asuihd auisuhdui'''
+
+# print(course)
+
+# index
+course = "math jhasv asuihd auisuhdui"
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[1:])
+print(course[:5])
+print(course[:])
+print(course[1:-1])
+# m
+# i
+# mat
+# math jhasv asuihd auisuhdui
+# ath jhasv asuihd auisuhdui
+# math
+# math jhasv asuihd auisuhdui
+# ath jhasv asuihd auisuhdu
