8610cabf9aa168882a0a7bc2382d794ca6bf78e8
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-08 19:02:53 +0530

    typecast

---
+++ day2/agecalculter.py
@@ -0,0 +1,11 @@
+# birth_year = input("Enter your birth year:  ")
+# age = 2026 - int(birth_year)
+# print(age )
+
+
+
+birth_year = input("Enter your birth year:  ")
+print(type(birth_year))
+age = 2026 - int(birth_year)
+print(type(age))
+print(age )
---
+++ day2/weight.py
@@ -0,0 +1,10 @@
+
+# pound to kg
+user_weight = int(input("Enter your weight in pounds: "))
+weight_in_kg = user_weight * 0.453592
+print(f"Your weight in kg is: {weight_in_kg}")
+
+# kg to pound
+user_weight_kg = int(input("Enter your weight in kg: "))
+weight_in_pounds = user_weight_kg / 0.453592
+print(f"Your weight in pounds is: {weight_in_pounds}")
