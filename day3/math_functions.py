24e3902f827b927e0476d7d774a8bacca9923bd0
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-09 21:31:55 +0530

    math function in python

---
+++ day3/math_functions.py
@@ -0,0 +1,22 @@
+import math
+
+
+# Number-theoretic functions
+print(math.sqrt(16))
+print(int(math.pow(2, 3)))
+print(math.comb(5, 2))
+print(math.factorial(5))
+print(math.gcd(48, 18))
+print(math.lcm(12, 15))
+print(math.perm(5, 2))
+
+print(math.dist((1, 2), (4, 6)))
+
+# 4.0
+# 8
+# 10
+# 120
+# 6
+# 60
+# 20
+# 5.0
