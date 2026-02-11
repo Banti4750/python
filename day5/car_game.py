2dcd4299d00732a1bfa2131555046dd9bfe2a95c
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-11 20:41:15 +0530

    ...

--- day5/car_game.py
+++ day5/car_game.py
@@ -2,13 +2,13 @@ cammand = ""
 
 while cammand.lower() != "quit":
     cammand = input(">").lower()
-    if cammand == "start":
+    if cammand.lower() == "start":
         print("Car started...Ready to go!")
-    elif cammand == "stop":
+    elif cammand.lower() == "stop":
         print("Car stopped.")
-    elif cammand == "help":
+    elif cammand.lower() == "help":
         print("""start - to start the car
         stop - to stop the car
         quit - to exit""")
-    elif cammand == "quit":
+    elif cammand.lower() == "quit":
         print("Exiting the game. Goodbye!")
