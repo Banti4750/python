75c1417779a5f562a9459d5d1339b2391a337ff7
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-13 12:59:43 +0530

    list method in python

--- day6/list.py
+++ day6/list.py
@@ -7,9 +7,34 @@
 # largest number in list
 numbers = [12 , 45 , 67 ,44]
 
-res = 0
-for items in numbers:
-    if items > res:
-        res = items
+# res = 0
+# for items in numbers:
+#     if items > res:
+#         res = items
 
-print(res)
+# print(res)
+
+# list method
+# numbers.append(111)
+numbers.insert(0 , 12)
+# numbers.remove(12)
+# numbers.clear()
+# numbers.pop()
+# print(numbers.index(12))
+# print(12 in numbers )
+# print(numbers.count(12))
+# numbers.sort()
+# numbers.reverse()
+numbers2 = numbers.copy()
+# print(numbers2)
+
+
+# remove dupliate from list
+
+numbers = [12 , 12 , 34 , 56 , 1 , 3 ,1 ]
+uniques = []
+for items in numbers:
+    if items not in uniques :
+        uniques.append(items)
+
+print(uniques)
