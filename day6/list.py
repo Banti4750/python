6f4db40b52aa869f5929085087f43b0b0e21ef6f
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-13 12:21:37 +0530

    list in python

---
+++ day6/list.py
@@ -0,0 +1,15 @@
+# names = ["banti" , "sam" , "suraj"]
+# print(names)
+# print(names[1])
+# print(names[0:2])
+
+
+# largest number in list
+numbers = [12 , 45 , 67 ,44]
+
+res = 0
+for items in numbers:
+    if items > res:
+        res = items
+
+print(res)
