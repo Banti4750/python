f2043199d5fa98465c66e540db122b6a1c9ca4b8
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-20 09:55:52 +0530

    attribute in python

--- day8/class.py
+++ day8/class.py
@@ -6,5 +6,11 @@ class Point:
         print("move")
 
 point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
 point1.draw()
 point1.move()
+
+point2 = Point()
+print(point2.x) # this will raise an error because x is not defined for point2
