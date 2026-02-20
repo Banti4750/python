938f2d7bb487e035c3a50cbcabc9a808eeb5cb0a
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-20 10:02:24 +0530

    constructer in python

--- day8/class.py
+++ day8/class.py
@@ -1,16 +1,44 @@
-class Point:
-    def draw(self):
-        print("draw")
+# class Point:
+#     def draw(self):
+#         print("draw")
 
-    def move(self):
-        print("move")
+#     def move(self):
+#         print("move")
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
-print(point1.x)
-point1.draw()
-point1.move()
+# point1 = Point()
+# point1.x = 10
+# point1.y = 20
+# print(point1.x)
+# point1.draw()
+# point1.move()
 
-point2 = Point()
-print(point2.x) # this will raise an error because x is not defined for point2
+# point2 = Point()
+# print(point2.x) # this will raise an error because x is not defined for point2
+
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def draw(self):
+#         print("draw")
+
+#     def move(self):
+#         print("move")
+
+# point1 = Point(10, 20)
+# point1.x = 30
+# print(point1.x)
+
+# exrecise
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"{self.name} is talking")
+
+person1 = Person("John")
+print(person1.name)
+person1.talk()
