6c549526f7e403c8ff37eb0d89a4ed6b3d93ce0f
Banti4750 <bantikumar6203818460@gmail.com>
2026-02-20 10:21:55 +0530

    inheritance in python

---
+++ day8/inheritance.py
@@ -0,0 +1,21 @@
+class Mammal:
+    def walk(self):
+        print("Walking")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("Barking")
+
+class Cat(Mammal):
+    def meow(self):
+        print("Meowing")
+
+
+dog1 = Dog()
+dog1.walk()
+dog1.bark()
+
+cat1 = Cat()
+cat1.walk()
+cat1.meow()
