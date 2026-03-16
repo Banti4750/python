eeb6f9a24cc2e426c99301aa098f4b92d0086de5
Banti4750 <bantikumar6203818460@gmail.com>
2026-03-16 16:30:49 +0530

    max element module

---
+++ day9/app.py
@@ -0,0 +1,10 @@
+from utils import max_element
+
+numbers = [3, 1, 4, 1, 5, 9]
+
+# by inbuilt max function
+max_value_builtin = max(numbers)
+
+max_value = max_element.maxi(numbers)
+print(f"The maximum value in the list is: {max_value}")
+print(f"The maximum value in the list is: {max_value}")
---
+++ day9/utils/max_element.py
@@ -0,0 +1,8 @@
+def maxi(numbers):
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
